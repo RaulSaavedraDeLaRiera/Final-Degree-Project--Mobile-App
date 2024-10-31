ef72a8aa090097289542b81f990ffc2ab1bfd441
RaulSRiera <rsaave02@ucm.es>
2024-10-31 14:16:19 +0100

    montados popups hotel

---
+++ TFG-Critterons-App/Assets/Hotel/HotelInput.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotelInput : MonoBehaviour
+{
+    //habra que cambiarlo a un popup segun tipo
+    [SerializeField]
+    ButtonActions critteronPopUp, ownedObjectPopUp, objectToBuyPopUp;
+
+    bool inputEnable = true;
+
+    public bool InputEnable
+    {
+        set { inputEnable  = value; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputEnable && Input.GetMouseButtonDown(0)) // Detecta clic izquierdo del mouse
+        {
+            TryInput();
+        }
+    }
+
+    void TryInput()
+    {
+     
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Realiza el raycast y verifica si impacta algún objeto con Collider
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject target = hit.collider.gameObject;
+
+            switch (target.tag)
+            {
+                case "Critteron":
+                    critteronPopUp.EnterAnimation();
+                    inputEnable = false;
+                    break;
+                case "OwnedObject":
+                    ownedObjectPopUp.EnterAnimation();
+                    inputEnable = false;
+                    break;
+                case "ObjectToBuy":
+                    objectToBuyPopUp.EnterAnimation();
+                    inputEnable = false;
+                    break;
+            }
+        }
+    }
+}
