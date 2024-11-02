241e6741988dcb304815dfb4cd77979796ebcb97
AntonioPove <79278677+AntonioPove@users.noreply.github.com>
2024-11-02 13:31:22 +0100

    Movimiento camara en el hotel y activacion objetos

---
+++ TFG-Critterons-App/Assets/Hotel/HotelMove.cs
@@ -0,0 +1,104 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Clase encargada del desplazamiento por la pantalla del hotel
+/// </summary>
+public class HotelMove : MonoBehaviour
+{
+    [SerializeField]
+    float hotelWidth;
+    [SerializeField]
+    float hotelHeight;
+
+    [SerializeField]
+    float moveSpeed;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+    private Vector3 lastPos;
+    private bool isDragging;
+
+    void Start()
+    {
+        isDragging = false;
+        moveSpeed /= 100;
+
+        float halfWidth = hotelWidth / 2f;
+        float halfHeight = hotelHeight / 2f;
+
+        // Definimos los limites de la pantalla
+        minX = -halfWidth;
+        maxX = halfWidth;
+        minY = -halfHeight;
+        maxY = halfHeight;
+
+    }
+
+    void Update()
+    {
+        // Editor de Unity
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartDragging(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0) && isDragging)
+        {
+            MoveCameraWithInput(Input.mousePosition);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        // En dispositivos android
+#elif UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                StartDragging(touch.position);
+            }
+            else if (touch.phase == TouchPhase.Moved && isDragging)
+            {
+                MoveCameraWithInput(touch.position);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                isDragging = false;
+            }
+        }
+#endif
+    }
+
+    private void StartDragging(Vector3 position)
+    {
+        lastPos = position;
+        isDragging = true;
+    }
+
+    private void MoveCameraWithInput(Vector3 currentPosition)
+    {
+        Vector3 touchDelta = currentPosition - lastPos;
+        Vector3 cameraMovement = new Vector3(-touchDelta.x * moveSpeed, -touchDelta.y * moveSpeed, 0);
+
+        moveCam(cameraMovement);
+        lastPos = currentPosition;
+    }
+
+    private void moveCam(Vector3 vector)
+    {
+        Vector3 newPosition = transform.position + vector;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+        newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
+
+        transform.position = newPosition;
+    }
+}
--- TFG-Critterons-App/Assets/Hotel/HotelObject.cs
+++ TFG-Critterons-App/Assets/Hotel/HotelObject.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class HotelObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Deberia irse y hacer que un manager le diga lo que tiene que monstrar directamente ??
+    // o que este a partir de la info que tiene cree el tipo de popup
+    [SerializeField]
+    GameObject popupPrefab;
+
+    Vector3 popupPos;
+    private bool isTouched;
+
+    // Estado del objeto
+    private bool isBougth;
+
+    private void Start()
     {
-        
+        popupPos = new Vector3 (0, 0, 0);
+        isTouched = false;
+
+        // Cambiar para que pida la info de si lo esta a un manager?
+        isBougth = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit) && hit.transform == transform)
+            {
+                ShowPopup();
+            }
+        }
+#elif UNITY_ANDROID
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit) && hit.transform == transform)
+                {
+                    isTouched = true;
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended && isTouched)
+            {
+                ShowPopup();
+                isTouched = false;
+            }
+        }
+#endif
+    }
+
+    private void ShowPopup()
+    {
+        if (popupPrefab != null)
+        {
+           // GameObject popUp = Instantiate(popupPrefab, transform.position + popupPos, Quaternion.identity);
+           // popUp.transform.SetParent(GameObject.Find("CanvasHotelUI").transform, false);
+        }
     }
 }
+
