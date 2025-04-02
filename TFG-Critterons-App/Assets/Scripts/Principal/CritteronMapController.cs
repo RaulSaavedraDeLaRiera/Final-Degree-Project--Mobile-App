538d33b39f0ab7a3e86298889bcb41ce1ac0d530
RaulSRiera <82366515+RaulSaavedraRiera@users.noreply.github.com>
2025-04-02 16:50:44 +0200

    añadido criteron (fijo) en principal

---
+++ TFG-Critterons-App/Assets/Scripts/Principal/CritteronMapController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.AdaptivePerformance.Provider.AdaptivePerformanceSubsystemDescriptor;
+
+public class CritteronMapController : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField]
+    Transform visualRoot;
+    [SerializeField]
+    GameObject plataform;
+    [SerializeField]
+    bool lookAtCamera;
+    
+    void Start()
+    {
+        //setear esta variable cuando se cambie critteron principal
+        var visual = visualRoot.Find(PlayerPrefs.GetString("currentCritteron", "Toadstool"));
+        animator = visual.GetComponent<Animator>();
+        visual.gameObject.SetActive(true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(plataform != null && lookAtCamera)
+        {
+            Vector3 direction = Camera.main.transform.position - transform.position;
+            direction.y = 0; // Mantiene el objeto recto
+            plataform.transform.rotation = Quaternion.LookRotation(direction);
+
+        }
+    }
+}
