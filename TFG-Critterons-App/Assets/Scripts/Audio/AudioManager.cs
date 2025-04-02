e0842096634a2fd7c5d3a07d4a5a40bf780a4955
RaulSRiera <82366515+RaulSaavedraRiera@users.noreply.github.com>
2025-04-02 17:16:47 +0200

    creada estructura del audiomanager

---
+++ TFG-Critterons-App/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager m;
+    [SerializeField]
+    AudioSource clickSound, winSound, defeatSound, criterronStopSound, combatSound, newLevelSound, upgradeSound, changeSceneSound, hitSoundBase;
+    [SerializeField]
+    int hitInstances;
+
+   
+    AudioSource[] hitSounds;
+
+    private void Awake()
+    {
+        if (m == null)
+        {
+            m = this;
+            DontDestroyOnLoad(gameObject);
+            LoadMultiSounds();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void LoadMultiSounds()
+    {
+        hitSounds = new AudioSource[hitInstances];
+        hitSounds[0] = hitSoundBase;
+        for (int i = 0; i < hitInstances; i++)
+        {
+            hitSounds[i] = Instantiate<GameObject>(hitSoundBase.gameObject, hitSoundBase.transform).GetComponent<AudioSource>();
+        }
+    }
+
+    public void PlaySound(string soundName, bool lowPriority = false)
+    {
+        AudioSource sound = null;
+
+        switch (soundName.ToLower())
+        {
+            case "click": sound = clickSound; break;
+            case "win": sound = winSound; break;
+            case "defeat": sound = defeatSound; break;
+            case "combat": sound = combatSound; break;
+            case "stop": sound = criterronStopSound; break;
+            case "newlevel": sound = newLevelSound; break;
+            case "upgrade": sound = upgradeSound; break;
+            case "changescene": sound = changeSceneSound; break;
+        }
+
+        if (sound != null && (!lowPriority || !sound.isPlaying))
+        {
+            sound.Play();
+        }
+    }
+
+
+    public void Hit()
+    {
+        foreach (var item in hitSounds)
+        {
+            if (!item.isPlaying)
+            {
+                item.Play();
+                break;
+            }
+        }
+    }
+}
